a9aa14f7460cf0dcf576a25c7ae909dbc752834a
snpk.hrak <ne240250e@gmail.com>
2020-01-17 00:09:36 +0900

    URLによって画面表示可変 日本語を表示するために文字コード指定

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var http = require('http');
 
 var server = http.createServer();
+var msg;
 
 server.on('request', function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('hello world');
+    switch (req.url) {
+        case '/taro':
+            msg = '僕の名前は田中太郎';
+            break;
+        case '/hanako':
+            msg = '私の名前は山田花子';
+            break;
+        default:
+            msg = '名前はまだない';
+            break;
+    }
+    res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
+    res.write(msg);
     res.end();
 });
 server.listen(8080);
